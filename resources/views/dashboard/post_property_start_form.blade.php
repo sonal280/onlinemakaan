074e4c47ba459b6871186fbd4aa746297a3aec1f
dgangolia <dgangolia@gmail.com>
2021-09-28 23:43:33 +0530

    new files created by dheeraj and changes

---
+++ resources/views/dashboard/post_property_start_form.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('title', 'Online Makaan || Post Property')
+@section('breadcrumb', 'Select Property Category')
+@section('heading', 'Post Property')
+@section('content')
+
+
+   
+  
+
+
+
+@endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -41,7 +41,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
             <div class="col-sm-6">
-                <h1>Profile</h1>
+                <h1>@yield('heading')</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
